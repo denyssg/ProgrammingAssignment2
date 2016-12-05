2b0efa241f9872e7c63796872dc8f8c3ef66cd26
denyssg <kdguilcazo@espe.edu.ec>
2016-12-05 11:28:28 -0500

    S

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##  Prueba
+##calcular la funcion para una matriz establecida en la funcion
+  ## I want to ask for help because i'm not an english native speaker
+  
+  ## La funci�n makeCacheMatrix crea un objeto especial denominado matriz
+  ##que puede guardar la inversa en el cach�, la cual, es una matriz invertible
+  
+  makeCacheMatrix <- function(a = matrix()) { 
+    #First we create this function that will help us through the environment.
+    
+    inversa <- NULL #Funcion inversa
+    y <- function(n) { # We create the function
+      a <<- d
+      inversa <<- NULL
+    }
+    get <- function() a #The commander get, will save the function
+    setinv <- function(inverse) inversa <<- inverse
+    getinv <- function() inversa
+    list(y = y, get = get, setinv = setinv, getinv = getinv)
+  }
+  
+  ## Write a short comment describing this function
+  ##We create the function cachesolve that calculates 
+  ## La funci�n cacheSolve calcula la inversa de la matriz
+  ## obtenida de la funci�n makeCacheMatrix. Si la inversa ya
+  ## ha sido calculada y la matriz no ha cambiado entonces devuelve
+  ## la inversa guardada en el cache
+  
+  
+  cacheSolve <- function(a, ...) { ## The use of ... are the parameters that we want to change.
+    # 
+    
+    inversa <- a$getinv()
+    
+    
+    if(!is.null(inversa)) {
+      print("Getting the result");
+      return(inversa)
+    }
+    data <- a$get()
+    
+    inversa <- solve(data, ...)
+    a$setinv(inversa)
+    inversa 
+  }
